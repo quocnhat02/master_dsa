8aaa3ad6ae50e65cf9ee24e62d18eba81f5206fe
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-19 17:10:49 +0700

    setup index.js of back-end

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(
+  express.json({
+    extended: true,
+    limit: "30mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "30mb",
+  })
+);
+app.use(cors());
