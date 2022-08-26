455df0f38b1b4915f91935a07b35541a9619429a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-26 21:54:51 +0700

    Campground model basic

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const Campground = require("./models/campground");
+
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Error connect to db ", err.message);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "cheap camping",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
