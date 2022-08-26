fe06d915487d45f2a044cc815fc942e8f2088be1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-26 23:26:21 +0700

    npm i method-override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 const mongoose = require("mongoose");
@@ -19,6 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
