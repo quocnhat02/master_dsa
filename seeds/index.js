101a93b7d2b52fd7cd7f2aa0b283e089bdb816df
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 12:14:28 +0700

    adding images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     let random1000 = Math.floor(Math.random() * 1000);
+    let price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "http://source.unsplash.com/collection/484351",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id rem excepturi perferendis maiores, ab dignissimos repellat voluptatum qui libero! Obcaecati natus laborum amet delectus sit incidunt asperiores quisquam ipsum cum.",
+      price,
     });
     await camp.save();
   }
