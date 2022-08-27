46e2d80586b4fa7010a8af77d664526f91978a48
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 10:49:48 +0700

    npm i ejs-mate

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
-const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -15,6 +16,7 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
