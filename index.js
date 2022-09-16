92ae16cce2a28d7c54435e1ad4342f0954290289
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-16 21:15:46 +0700

    prime

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function factorial(n) {
-  if (n <= 1) {
-    return 1;
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else if (n % 2 === 0) {
+    return false;
+  } else {
+    let halfNumber = Math.sqrt(n);
+    for (let i = 3; i < halfNumber; i += 2) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
   }
-  return factorial(n - 1) * n;
 }
 
-console.log(factorial(0));
-console.log(factorial(1));
-console.log(factorial(5));
+console.log(isPrime(1));
+console.log(isPrime(4));
+console.log(isPrime(5));
+console.log(isPrime(113));
