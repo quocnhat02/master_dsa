b3d2d87b217ef17504d0d8ced4a5462f9f63e7f4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-16 23:25:37 +0700

    linearsearch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function recursiveFibonacci(n) {
-  if (n < 2) {
-    return n;
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
   }
-  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+  return -1;
 }
 
-console.log(recursiveFibonacci(0));
-console.log(recursiveFibonacci(1));
-console.log(recursiveFibonacci(6));
+console.log(linearSearch([-5, 2, 10, 4, 6], 10));
+console.log(linearSearch([-5, 2, 10, 4, 6], 6));
+console.log(linearSearch([-5, 2, 10, 4, 6], 20));
