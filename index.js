413e74ef73b8d39e411707afd356a6e5c6800e77
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-16 21:44:50 +0700

    poweroftwo

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function isPrime(n) {
-  if (n < 2) {
+function isPowerOfTwo(n) {
+  if (n < 1) {
     return false;
-  } else if (n === 2) {
-    return true;
-  } else if (n % 2 === 0) {
-    return false;
-  } else {
-    let halfNumber = Math.sqrt(n);
-    for (let i = 3; i < halfNumber; i += 2) {
-      if (n % i === 0) {
-        return false;
-      }
-    }
-    return true;
   }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n /= 2;
+  }
+  return true;
 }
 
-console.log(isPrime(1));
-console.log(isPrime(4));
-console.log(isPrime(5));
-console.log(isPrime(113));
+function numberPowerOfTwo(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return numberPowerOfTwo(n - 1) * 2;
+}
+console.log(numberPowerOfTwo(3));
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(5));
