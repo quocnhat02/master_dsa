de70ac8c5754b27111a5dc8aee538c3708a6f683
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-16 23:45:56 +0700

    binarysearch

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
+function binarySearch(arr, n, t, l = 0, r = n - 1) {
+  let m = Number.parseInt((l + r) / 2);
+  if (arr[m] === t) {
+    return m;
+  } else if (arr[m] > t) {
+    return binarySearch(arr, n, t, 0, m - 1);
+  } else if (arr[m] < t) {
+    return binarySearch(arr, n, t, m + 1, n);
+  } else {
+    return -1;
   }
-  return -1;
 }
 
-console.log(linearSearch([-5, 2, 10, 4, 6], 10));
-console.log(linearSearch([-5, 2, 10, 4, 6], 6));
-console.log(linearSearch([-5, 2, 10, 4, 6], 20));
+function swap(a, b) {
+  let temp = a;
+  a = b;
+  b = temp;
+}
+
+function sortArray(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        swap(arr[i], arr[j]);
+      }
+    }
+  }
+}
+
+let arr = [-5, 2, 4, 6, 10];
+console.log(binarySearch(arr, arr.length, 10, 0, arr.length - 1));
+console.log(binarySearch(arr, arr.length, 6, 0, arr.length - 1));
+console.log(binarySearch(arr, arr.length, 20, 0, arr.length - 1));
