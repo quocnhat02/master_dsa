0329b9ed233e8380ca86a30d8b4938aee0537e0b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 09:57:24 +0700

    recursivebinarysearch

--- index.js
+++ index.js
@@ -11,22 +11,43 @@
 //   }
 // }
 
-function binarySearch(arr, target) {
-  let leftIndex = 0;
-  let rightIndex = arr.length - 1;
+// function binarySearch(arr, target) {
+//   let leftIndex = 0;
+//   let rightIndex = arr.length - 1;
 
-  while (leftIndex <= rightIndex) {
-    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-    if (target === arr[middleIndex]) {
-      return middleIndex;
-    } else if (target < arr[middleIndex]) {
-      rightIndex = middleIndex - 1;
-    } else {
-      leftIndex = middleIndex + 1;
-    }
+//   while (leftIndex <= rightIndex) {
+//     let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+//     if (target === arr[middleIndex]) {
+//       return middleIndex;
+//     } else if (target < arr[middleIndex]) {
+//       rightIndex = middleIndex - 1;
+//     } else {
+//       leftIndex = middleIndex + 1;
+//     }
+//   }
+
+//   return -1;
+// }
+
+function recursiveBinarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
   }
 
-  return -1;
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (target === arr[middleIndex]) {
+    return middleIndex;
+  }
+
+  if (target < arr[middleIndex]) {
+    return search(arr, target, leftIndex, middleIndex - 1);
+  } else {
+    return search(arr, target, middleIndex + 1, rightIndex);
+  }
 }
 
 function swap(a, b) {
@@ -46,6 +67,6 @@ function sortArray(arr) {
 }
 
 let arr = [-5, 2, 4, 6, 10];
-console.log(binarySearch(arr, 10));
-console.log(binarySearch(arr, 6));
-console.log(binarySearch(arr, 20));
+console.log(recursiveBinarySearch(arr, 10));
+console.log(recursiveBinarySearch(arr, 6));
+console.log(recursiveBinarySearch(arr, 20));
