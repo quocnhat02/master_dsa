7e755812e5d632893fac596fddb0e555e3d07fe9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 17:37:33 +0700

    insertionsort

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-function bubbleSort(arr) {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-  } while (swapped);
+    arr[j + 1] = numberToInsert;
+  }
 }
 
 const array = [20, 16, -8, 2, 7, 4];
-// 16,-8,2,7,4,20
-bubbleSort(array);
+// 20, 16, -8, 2, 7, 4 NTS=16, SE=20
+// 16, 20, -8, 2, 7, 4 NTS=20 SE=
 
+insertionSort(array);
 console.log(array);
