2994b882f8de2426150e8dfb6b87601720e53123
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 23:42:33 +0700

    O(N) 2

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function funChallenge(input) {
-  let a = 10; // O(1)
-  a = 50 + 3; // O(1)
-
-  for (let i = 0; i < input.length; i++) {
-    // O(n)
-    anotherFunction(); // O(n)
-    const stranger = true; // O(n)
-    a++; // O(n)
+function anotherFunChallenge(input) {
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+  for (let i = 0; i < input; i++) {
+    let x = i + 5; // O(n)
+    let y = i + 10; // O(n)
+    let z = i + 50; // O(n)
   }
-  return a; // O(1)
+  for (let j = 0; j < input; j++) {
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
+  }
+  let whoAmI = "I don't know"; // O(1)
 }
 
-// BIG O(n)
+// BIG O (4 + 5n)
