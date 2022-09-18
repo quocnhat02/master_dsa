055991cf1922c56d1c890bd28627517446d787dd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 23:35:02 +0700

    O(N)

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// const nemo = ['nemo'];
-// const everyone = ['chick', 'john', 'shark', 'doran', 'nemo'];
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
 
-// const large = new Array(10).fill('nemo');
-
-// function findNemo(array) {
-//   let t0 = performance.now();
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found NEMO!');
-//     }
-//   }
-//   let t1 = performance.now();
-//   console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
-// }
-
-// findNemo(large); // O(n) -> Linear Time
-
-const boxes = [0, 1, 2, 3, 4, 5];
-
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]); // O(1)
-  console.log(boxes[1]); // O(1)
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n)
+    const stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
 }
 
-logFirstTwoBoxes(boxes);
+// BIG O(n)
