c1eb9b012e8e4f37e601c2032c4985566482e2f6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 23:24:32 +0700

    O(1)

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const nemo = ['nemo'];
-const everyone = ['chick', 'john', 'shark', 'doran', 'nemo'];
+// const nemo = ['nemo'];
+// const everyone = ['chick', 'john', 'shark', 'doran', 'nemo'];
 
-const large = new Array(10).fill('nemo');
+// const large = new Array(10).fill('nemo');
 
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO!');
-    }
-  }
-  let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
+// function findNemo(array) {
+//   let t0 = performance.now();
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO!');
+//     }
+//   }
+//   let t1 = performance.now();
+//   console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
+// }
+
+// findNemo(large); // O(n) -> Linear Time
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
 }
 
-findNemo(large);
+logFirstTwoBoxes(boxes);
