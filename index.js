41edbf73e804d2da2f6866636686411a9746da42
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 20:14:55 +0700

    big o and scalability 1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const nemo = ['nemo'];
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
 }
 
 findNemo(nemo);
