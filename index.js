c1be827261a2a3eb1b4d754e2a42f132f6738601
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 20:07:58 +0700

    what is good code

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-function linearSearch(arr, x) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === x) {
-      return i;
+const nemo = ['nemo'];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
     }
   }
-  return -1;
 }
 
-const linearSearchRecursive = (arr, n, x) =>
-  n === 0 ? -1 : arr[n] === x ? n : linearSearchRecursive(arr, n - 1, x);
-
-const arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170];
-// console.log(linearSearch(arr, 175));
-console.log(linearSearchRecursive(arr, arr.length - 1, 80));
-
-// Time complexity: O(N)
-// Auxiliary Space: O(1)
+findNemo(nemo);
