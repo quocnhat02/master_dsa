94125a76465e6c8e7d6fab300b00c05b6556fc75
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 13:55:03 +0700

    binarySearch1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-'helloworldmyfriends'.length; // O(1)
+function binarySearch(
+  arr,
+  target,
+  left = 0,
+  right = arr.length - 1,
+  mid = Math.floor((left + right) / 2)
+) {
+  const start = performance.now();
+  do {
+    mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      console.log(performance.now() - start);
+      return mid;
+    }
+    if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  } while (left <= right);
+  console.log(performance.now() - start);
+
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
