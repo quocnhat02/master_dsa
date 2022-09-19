2c4a52d0475dde9ff2235e482c56d1a199f98528
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 15:27:11 +0700

    ternarySearch2.1

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ function ternarySearch2(
   return -1;
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const mang = [];
+for (let index = 1; index <= 10; index++) {
+  mang.push(index);
+}
 
-console.log(ternarySearch2(array, 8));
+console.log(ternarySearch2(mang, 4509));
