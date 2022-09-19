0dddb10f9ad6cf3e1e6889e9bdf984dd4ea5c003
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 21:13:48 +0700

    jumpsearch

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-function jumpSearch(arr, x, n) {
-  let step = Math.sqrt(n);
+const strings = ['a', 'b', 'c', 'd'];
 
-  let prev = 0;
-  while (arr[Math.min(step, n) - 1] < x) {
-    prev = step;
-    step += Math.sqrt(n);
-    if (prev >= n) {
-      return -1;
-    }
-  }
-
-  while (arr[prev] < x) {
-    prev++;
-    if (prev === Math.min(step, n)) {
-      return -1;
-    }
-  }
-
-  if (arr[prev] === x) {
-    return prev;
-  }
-
-  return -1;
-}
-
-let arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610];
-
-console.log(jumpSearch(arr, 500, arr.length));
+console.log(strings[2]);
