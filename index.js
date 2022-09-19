a6e8e109218c1bb4634c80f6d40245d06b8b19a6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 12:57:48 +0700

    exercise space complexity

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function anotherFunChallenge(input) {
-  let a = 5; // O(1)
-  let b = 10; // O(1)
-  let c = 50; // O(1)
-  for (let i = 0; i < input; i++) {
-    let x = i + 5; // O(n)
-    let y = i + 10; // O(n)
-    let z = i + 50; // O(n)
+function boooo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log('boooooooo!');
   }
-  for (let j = 0; j < input; j++) {
-    let p = j * 2; // O(n)
-    let q = j * 2; // O(n)
-  }
-  let whoAmI = "I don't know"; // O(1)
 }
 
-// BIG O (4 + 5n)
+// boooo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+console.log(arrayOfHiNTimes(6)); // O(n)
