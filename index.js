96ca44f58fe841eedaeb5a45027ee3ed0f7bca12
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 21:31:59 +0700

    arrays 3

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const strings = ['a', 'b', 'c', 'd'];
 
+// function addFirstArray(arr, newItem) {
+//   for (let i = arr.length; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+//   }
+//   arr[0] = newItem;
+//   return arr;
+// }
+
 // push
 // strings.push('e'); // O(1)
 
 // pop
-strings.pop();
+// strings.pop(); // O(1)
+
+// unshift
+// strings.unshift('z'); // O(n)
+
+// splice
+strings.splice(2, 0, 'alien');
 
 console.log(strings);
