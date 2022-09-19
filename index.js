94670f7322b10aefef222746d7d7f25749f503d9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-19 19:22:33 +0700

    containsCommonItem123

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-function containsCommonItem(arr1, arr2) {
-  let map = {};
+function containsCommonItem1(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
-    if (!map[arr1[i]]) {
-      const item = arr1[i];
-      map[item] = true;
-    }
-  }
-
-  for (let j = 0; j < arr2.length; j++) {
-    if (map[arr2[j]]) {
-      return true;
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
     }
   }
   return false;
 }
 
-const arr1 = ['a', 'b', 'c', 'd'];
-const arr2 = ['w', 'c', 'r', 'x'];
+// O(n^2)
 
-console.log(containsCommonItem(arr1, arr2));
+function containsCommonItem2(arr1, arr2) {
+  const object = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if (!object[arr1[i]]) {
+      const item = arr1[i];
+      object[item] = true;
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (object[arr2[j]]) {
+      return true;
+    }
+  }
+  return -1;
+}
+
+// O(a + b)
+
+function containsCommonItem3(arr1, arr2) {
+  return arr1.some((item) => arr2.includes(item));
+}
+
+const arr1 = ['1', 'b', 'c', 'd'];
+const arr2 = [1, 'cd', 'r', 'x'];
+
+console.log(containsCommonItem1(arr1, arr2));
+console.log(containsCommonItem2(arr1, arr2));
+console.log(containsCommonItem3(arr1, arr2));
