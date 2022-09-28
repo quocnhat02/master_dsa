5c476d93c93ed4dc63bb9c921534ca6b4b4f54d7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 22:59:51 +0700

    array 3

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const strings = ['a', 'b', 'c', 'd'];
 
 // push
-strings.push('e'); // O(1)
+// strings.push('e'); // O(1)
+
+// pop
+// strings.pop(); // O(1)
+
+// unshift
+// strings.unshift('x'); // O(n)
+
+// splice
+strings.splice(2, 0, 'alien');
 
 console.log(strings);
