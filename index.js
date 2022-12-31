004a2b8f72d6d8a01a41d6c2ba65252ee0f77b0e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-31 19:36:37 +0700

    Flatten Array

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// T:O(n^2) S:O(1)
-function isUnique(str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str.lastIndexOf(str[i]) != i) {
-      return false;
-    }
-  }
-  return true;
-}
-
 // T:O(n*log(n)) S:O(n)
-function isBOUnique(str) {
+function isUnique(str) {
   let chars = str.split('').sort();
 
   for (let i = 1; i < chars.length; i++) {
@@ -20,54 +10,24 @@ function isBOUnique(str) {
   return true;
 }
 
-// T:O(n) S:O(n)
-function isOUnique(str) {
-  let chars = {};
+function flatten(nestedArray) {
+  const newArray = [];
 
-  for (let i = 0; i < str.length; i++) {
-    const thisChar = str[i];
+  for (let i = 0; i < nestedArray.length; i++) {
+    const thisItem = nestedArray[i];
 
-    if (chars[thisChar]) {
-      return false;
+    if (Array.isArray(thisItem)) {
+      const flatItem = flatten(thisItem);
+
+      for (let j = 0; j < flatItem.length; j++) {
+        newArray.push(flatItem[j]);
+      }
+    } else {
+      newArray.push(thisItem);
     }
-
-    chars[thisChar] = true;
   }
-  return true;
+
+  return newArray;
 }
 
-// T:O(n) S:O(n)
-function isSetUnique(str) {
-  let chars = new Set();
-
-  for (let i = 0; i < str.length; i++) {
-    const thisChar = str[i];
-
-    if (chars.has(thisChar)) {
-      return false;
-    }
-
-    chars.add(thisChar);
-  }
-  return true;
-}
-
-// T:O(n) S:O(n)
-function isSSetUnique(str) {
-  return new Set(str).size === str.length;
-}
-
-// console.log(isUnique('abc'));
-// console.log(isUnique('abca'));
-
-// console.log(isOUnique('abc'));
-// console.log(isOUnique('abca'));
-
-// console.log(isBOUnique('abc'));
-// console.log(isBOUnique('abca'));
-
-console.log(isSetUnique('abc'));
-console.log(isSetUnique('abca'));
-
-console.log(isSSetUnique('abc'));
-console.log(isSSetUnique('abca'));
+console.log(flatten([[[[1], 2], 3], [4], [], [[5]]]));
