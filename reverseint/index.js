89ec4536ce4a743ea71884ba591b7884917500fe
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-10 18:59:46 +0700

    reverse int 1

--- reverseint/index.js
+++ reverseint/index.js
@@ -1,4 +1,9 @@
-const reverseInt = (num) =>
-  Number.parseInt(num.toString().split('').reverse().join(''));
+const reverseInt = (num) => {
+  if (Math.sign(num) === -1) {
+    return Number.parseInt('-' + num.toString().split('').reverse().join(''));
+  } else {
+    return Number.parseInt(num.toString().split('').reverse().join(''));
+  }
+};
 
-console.log(reverseInt(123));
+console.log(reverseInt(-123));
