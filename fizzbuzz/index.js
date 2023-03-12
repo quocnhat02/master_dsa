5f86f9d8e5f16b4029fbb6f99fb9f5804e6de753
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-12 11:29:28 +0700

    fizzbuzz

---
+++ fizzbuzz/index.js
@@ -0,0 +1,13 @@
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+      console.log('fizz');
+    }
+    if (i % 5 === 0) {
+      console.log('buzz');
+    }
+    console.log(i);
+  }
+};
+
+fizzBuzz(10);
