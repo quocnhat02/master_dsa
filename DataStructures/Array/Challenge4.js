5a22b5d37c6dca7869e31538ebbabe97aa4a7158
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-14 18:44:43 +0700

    Array of Products of All Elements 3

--- DataStructures/Array/Challenge4.js
+++ DataStructures/Array/Challenge4.js
@@ -11,6 +11,28 @@ function findProduct1(arr) {
   return result;
 }
 
+function findProduct2(arr) {
+  let t = 1,
+    result = [];
+  for (let i = 0; i < arr.length; i++) {
+    t *= arr[i];
+  }
+
+  for (let j = 0; j < arr.length; j++) {
+    result.push(t / arr[j]);
+  }
+
+  return result;
+}
+
+function findProduct3(arr) {
+  let t = arr.reduce((acc, cur) => acc * cur, 1);
+
+  return arr.map((val) => t / val);
+}
+
 let arr = [1, 2, 3, 4];
 
 console.log(findProduct1(arr));
+console.log(findProduct2(arr));
+console.log(findProduct3(arr));
