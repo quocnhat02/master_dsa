896129edd19d9e678eb2f87dc435cb57ac29a78f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-14 16:26:14 +0700

    mergeArrayOptimize

--- DataStructures/Array/Challenge2.js
+++ DataStructures/Array/Challenge2.js
@@ -1,30 +1,24 @@
 // Challenge 2: Merge Two Sorted Arrays
 
 function mergeArrays1(arr1, arr2) {
-  let arr = [];
+  let result = [];
+
   let i = 0,
     j = 0;
-  let lengthArr1 = arr1.length;
-  let lengthArr2 = arr2.length;
-  while (i < lengthArr1 && j < lengthArr2) {
+
+  while (i < arr1.length && j < arr2.length) {
     if (arr1[i] > arr2[j]) {
-      arr.push(arr2[j]);
+      result.push(arr2[j]);
       j++;
     } else {
-      arr.push(arr1[i]);
+      result.push(arr1[i]);
       i++;
     }
   }
 
-  if (i <= lengthArr1 - 1) {
-    arr1.splice(0, i);
-    arr = arr.concat(arr1);
-  } else if (j <= lengthArr2 - 1) {
-    arr2.splice(0, j);
-    arr = arr.concat(arr2);
-  }
+  result = result.concat(arr1.slice(i)).concat(arr2.slice(j));
 
-  return arr;
+  return result;
 }
 
 const mergeArrays2 = (arr1, arr2) => [...arr1, ...arr2].sort();
@@ -33,3 +27,4 @@ let arr1 = [1, 3, 4, 5];
 let arr2 = [2, 6, 7, 8];
 
 console.log(mergeArrays1(arr1, arr2));
+console.log(mergeArrays2(arr1, arr2));
