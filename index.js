0ee9d78839852b021a10d830258d1f913e0f7b11
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-25 23:08:36 +0700

    item contain array 2

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function containItem1(arr1, arr2) {
 }
 
 // O(n^2)
+// O(1) Space Complexity
 
 function containItem2(arr1, arr2) {
   let obj = {};
@@ -29,9 +30,15 @@ function containItem2(arr1, arr2) {
 }
 
 // O(n)
+// O(n) Space Complexity
+
+function containItem3(arr1, arr2) {
+  return arr1.some((item) => arr2.includes(item));
+}
 
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 4];
 
 console.log(containItem1(arr1, arr2));
 console.log(containItem2(arr1, arr2));
+console.log(containItem3(arr1, arr2));
