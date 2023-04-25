013df4dce5220066c3ad03224c9da4d602adc196
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-25 22:46:47 +0700

    item contain array

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const nemo = ['nemo'];
-const everyone = new Array(100).fill('nemoTest');
-everyone.push('nemo');
-
-function findNemo(array) {
-  let t0 = performance.now();
-  let t1;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      t1 = performance.now() - t0;
-      console.log('Found Nemo!');
+function containItem1(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
     }
   }
-  console.log('Call to find Nemo took ' + t1 + 'ms');
+
+  return false;
 }
 
-findNemo(nemo);
-findNemo(everyone);
+// O(n^2)
+
+function containItem2(arr1, arr2) {
+  let obj = {};
+
+  arr1.forEach((element) => {
+    obj[element] = true;
+  });
+
+  for (let element of arr2) {
+    if (obj[element]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// O(n)
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6, 7, 4];
+
+console.log(containItem1(arr1, arr2));
+console.log(containItem2(arr1, arr2));
