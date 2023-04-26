edd10fdffb206e3b0669886aec71fd1d5ac1045b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-26 23:38:25 +0700

    first recurring character

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-class HashTable {
-  constructor(size) {
-    this.data = new Array(size);
-  }
+function findFirstRepeatNumber(array) {
+  let map = {};
 
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+  for (let i = 0; i < array.length; i++) {
+    if (map[array[i]]) {
+      return array[i];
     }
-    return hash;
+    map[array[i]] = true;
   }
 
-  set(key, value) {
-    let address = this._hash(key);
-    if (!this.data[address]) {
-      this.data[address] = [];
-    }
-    this.data[address].push([key, value]);
-  }
-
-  get(key) {
-    let address = this._hash(key);
-    const currentBucket = this.data[address];
-    if (currentBucket) {
-      for (let i = 0; i < currentBucket.length; i++) {
-        if (currentBucket[i][0] === key) {
-          return currentBucket[i][1];
-        }
-      }
-    }
-    return undefined;
-  }
-
-  keys() {
-    const keysArray = [];
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        keysArray.push(this.data[i][0][0]);
-      }
-    }
-
-    return keysArray;
-  }
+  return undefined;
 }
 
-const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 1000);
-myHashTable.set('apples', 54);
-myHashTable.set('oranges', 7);
-console.log(myHashTable.keys());
+let arr1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+let arr2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
+let arr3 = [1, 2, 3];
+
+console.log(findFirstRepeatNumber(arr1));
+console.log(findFirstRepeatNumber(arr2));
+console.log(findFirstRepeatNumber(arr3));
