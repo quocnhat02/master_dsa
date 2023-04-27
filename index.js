419700ff5ba9e46fa23e1e4d92734800b44e3c43
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-27 22:59:19 +0700

    link list 3

--- index.js
+++ index.js
@@ -1,27 +1,86 @@
-// let myLinkedList = {
-//   head: {
-//     value: 10,
-//     next: {
-//       value: 5,
-//       next: {
-//         value: 16,
-//         next: null,
-//       },
-//     },
-//   },
-// };
-
-class LinkedList {
+class Node {
   constructor(value) {
-    this.head = {
-      value,
-      next: null,
-    };
-    this.tail = this.head;
-    this.length = 1;
+    this.value = value;
+    this.next = null;
   }
 }
 
-let myLinkedList2 = new LinkedList(10);
+class LinkList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
 
-console.log(myLinkedList2);
+  printList() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  traverseToIndex(index) {
+    let currentNode = this.head;
+    let counter = 0;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index <= 0) {
+      return this.prepend(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    if (index >= this.length) {
+      index = this.length - 1;
+    }
+    let leader = this.traverseToIndex(index - 1);
+    let unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkList = new LinkList(1);
+myLinkList.append(2);
+myLinkList.append(3);
+myLinkList.prepend(0);
+// myLinkList.insert(2, 99);
+myLinkList.remove(3);
+console.log(myLinkList.printList());
