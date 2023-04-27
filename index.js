f0a7874d385d58b6f9ff79ed8582f46fc6fbd6a8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-27 19:34:34 +0700

    link list 1

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-function findFirstRepeatNumber(array) {
-  let map = {};
+const basket = ['apples', 'grapes', 'pears'];
 
-  for (let i = 0; i < array.length; i++) {
-    if (map[array[i]]) {
-      return array[i];
-    }
-    map[array[i]] = true;
-  }
-
-  return undefined;
-}
-
-let arr1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-let arr2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
-let arr3 = [1, 2, 3];
-
-console.log(findFirstRepeatNumber(arr1));
-console.log(findFirstRepeatNumber(arr2));
-console.log(findFirstRepeatNumber(arr3));
+let obj1 = { a: true };
+let obj2 = obj1;
+obj1.a = 'hello';
+delete obj1;
+obj2 = 'hello2';
+console.log('2', obj2);
