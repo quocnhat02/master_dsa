651ad018a01e33c98cc14e4fab587daa0a41c4a1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-30 15:27:56 +0700

    remove even number

--- DataStructures/Array/Challenge1.js
+++ DataStructures/Array/Challenge1.js
@@ -1,32 +1,22 @@
 // Challenge 1: Remove Even Integers From an Array
 
 function removeEven(arr) {
-  let newArr = [];
-  arr.forEach((element) => {
-    if (element % 2 != 0) {
-      newArr.push(element);
-    }
-  });
+  const oddArray = [];
 
-  return newArr;
+  for (let number of arr) {
+    if (number % 2 !== 0) {
+      oddArray.push(number);
+    }
+  }
+
+  return oddArray;
 }
 
-const removeEven2 = (arr) => arr.filter((e) => e % 2 !== 0);
-
-function removeEven3(arr) {
-  let writeIndex = 0;
-
-  arr.forEach((e, i) => {
-    if (e % 2 !== 0) {
-      arr[writeIndex] = e;
-      writeIndex++;
-    }
-  });
-
-  arr.length = writeIndex;
-  return arr;
+function removeEven2(arr) {
+  return arr.filter((number) => number % 2 !== 0);
 }
 
-console.log(removeEven([1, 2, 4, 5, 10, 6, 3]));
-console.log(removeEven2([1, 2, 4, 5, 10, 6, 3]));
-console.log(removeEven3([1, 2, 4, 5, 10, 6, 3]));
+let arr = [1, 2, 4, 5, 10, 6, 3];
+
+console.log(removeEven(arr));
+console.log(removeEven2(arr));
