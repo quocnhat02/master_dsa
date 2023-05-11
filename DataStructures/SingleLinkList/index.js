7e2bd47f5f26d00b3242e3da8dc5218472bf95f3
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-11 20:17:09 +0700

    link list 1

--- DataStructures/SingleLinkList/index.js
+++ DataStructures/SingleLinkList/index.js
@@ -24,12 +24,46 @@ class LinkedList {
     }
     this.length++;
   }
+
+  insertAtTail(value) {
+    let newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.nextElement = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  search(value) {
+    if (this.length === 0) {
+      return false;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.nextElement !== null) {
+      if (currentNode.data === value) {
+        return true;
+      }
+      currentNode = currentNode.nextElement;
+    }
+
+    if (currentNode.data === value) {
+      return true;
+    }
+
+    return false;
+  }
 }
 
 let linkedList = new LinkedList();
 
-linkedList.insertAtHead(1);
-linkedList.insertAtHead(2);
-linkedList.insertAtHead(3);
+linkedList.insertAtTail(1);
+linkedList.insertAtTail(2);
+linkedList.insertAtTail(3);
 
-console.log(linkedList);
+console.log(linkedList.search(3));
