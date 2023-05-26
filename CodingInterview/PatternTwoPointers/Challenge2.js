aae08fca25f0f4cfbbbb923b00f4957f44c218bb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-26 08:43:26 +0700

    Remove Duplicates (easy) 2

--- CodingInterview/PatternTwoPointers/Challenge2.js
+++ CodingInterview/PatternTwoPointers/Challenge2.js
@@ -21,5 +21,25 @@ function removeDuplicates(array) {
   return Object.keys(map).length;
 }
 
-console.log(removeDuplicates([2, 3, 3, 3, 6, 9, 9])); // 4
-console.log(removeDuplicates([2, 2, 2, 11])); // 2
+function removeDuplicates2(array) {
+  let start = 0;
+  let end = array.length - 1;
+  let sum = 0;
+  let result = [];
+
+  while (end >= start) {
+    if (array[start] !== array[start + 1]) {
+      sum++;
+      result.push(array[start]);
+    }
+    start++;
+  }
+
+  return result;
+}
+
+// console.log(removeDuplicates([2, 3, 3, 3, 6, 9, 9])); // 4
+// console.log(removeDuplicates([2, 2, 2, 11])); // 2
+
+console.log(removeDuplicates2([2, 3, 3, 3, 6, 9, 9])); // 4
+console.log(removeDuplicates2([2, 2, 2, 11])); // 2
