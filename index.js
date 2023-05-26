17bcc8e2e2b8b5a8ac7d5d897aafe6943238bb67
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-26 10:16:41 +0700

    rectangle pattern

--- index.js
+++ index.js
@@ -54,7 +54,29 @@ function printCharacterPattern(n) {
   }
 }
 
+function printRectanglePattern(r, c) {
+  let star = ' * ';
+  let line = '';
+
+  for (let i = 0; i < r; i++) {
+    line = '';
+    for (let j = 0; j < c; j++) {
+      if (i === 0 || i === r - 1) {
+        line += star;
+      } else {
+        if (j === 0 || j === c - 1) {
+          line += star;
+        } else {
+          line += '   ';
+        }
+      }
+    }
+    console.log(line);
+  }
+}
+
 // printStarPattern(4);
 // printInvertedStartPattern(4);
 // printHalfPyramidPattern(4);
 // printCharacterPattern(4);
+printRectanglePattern(4, 6);
