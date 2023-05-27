3a50d206317443c04d1a669db48816a0c17b3f55
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-27 14:14:40 +0700

    largest number

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ function linearSearch(array, target) {
   return -1;
 }
 
-console.log(linearSearch([1, 2, 3, 4, 5], 6));
+// console.log(linearSearch([1, 2, 3, 4, 5], 6));
+
+function largestNumber(array) {
+  let max = array[0];
+
+  for (let index = 1; index < array.length; index++) {
+    if (max < array[index]) {
+      max = array[index];
+    }
+  }
+
+  return max;
+}
+
+console.log(largestNumber([1, 2, 5, 8, 7]));
