5af7e06dd7e9fad5e6ba0b9aed9d2486f7676f2e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-07-30 11:49:51 +0700

    const var let

--- data_structers/index.js
+++ data_structers/index.js
@@ -1,4 +1,8 @@
-let array = [1, 3, 5, 'seven'];
-console.log(array);
-delete array[3];
-console.log(array);
+function foo() {
+  var n = 6;
+  console.log(n);
+}
+
+var n = 5;
+console.log(n);
+foo();
