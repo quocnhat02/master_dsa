5801c4ee11c785bb77c6ec7afd6fdc76a9b0c4b4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-28 22:12:54 +0700

    link list 3

--- datastructers/linklist/index.js
+++ datastructers/linklist/index.js
@@ -13,4 +13,30 @@ class LinkedList {
   isEmpty() {
     this.head === null ? true : false;
   }
+
+  insertAtHead(data) {
+    if (this.isEmpty()) {
+      this.head = new Node(data);
+    } else {
+      const newNode = new Node(data);
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  print() {
+    let pos = this.head;
+    while (pos !== null) {
+      console.log(pos);
+      pos = pos.next;
+    }
+  }
 }
+
+const linklist = new LinkedList();
+
+linklist.insertAtHead(1);
+linklist.insertAtHead(2);
+linklist.insertAtHead(3);
+
+linklist.print();
