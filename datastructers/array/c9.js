3168ce1a9861d18e58415820d73b4fcc14667069
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-28 19:45:33 +0700

    Challenge 9: Rearrange Positive & Negative Values 1

--- datastructers/array/c9.js
+++ datastructers/array/c9.js
@@ -1,6 +1,6 @@
 // Challenge 9: Rearrange Positive & Negative Values
 
-function reArrange(arr) {
+function reArrange1(arr) {
   let negArr = [];
   let posArr = [];
 
@@ -15,6 +15,25 @@ function reArrange(arr) {
   return negArr.concat(posArr);
 }
 
+function reArrange2(arr) {
+  let leftMostPosEle = 0;
+  let tmp;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      if (i !== leftMostPosEle) {
+        tmp = arr[i];
+        arr[i] = arr[leftMostPosEle];
+        arr[leftMostPosEle] = tmp;
+      }
+      leftMostPosEle++;
+    }
+  }
+
+  return arr;
+}
+
 const arr = [10, -1, 20, 4, 5, -9, -6];
 
-console.log(reArrange(arr));
+console.log(reArrange1(arr));
+console.log(reArrange2(arr));
