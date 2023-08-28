b80a4db6524819d1fa9839e77ff559175237b6be
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-28 14:32:19 +0700

    Challenge 3: Find Two Numbers that Add up to value

---
+++ datastructers/array/c3.js
@@ -0,0 +1,58 @@
+// Challenge 3: Find Two Numbers that Add up to "value"
+
+function binarySearch(arr, target) {
+  let start = 0,
+    end = arr.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
+function findSum1(arr, value) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    const found = binarySearch(arr, value - arr[i]);
+
+    if (found !== -1) {
+      return [arr[i], arr[found]];
+    }
+  }
+
+  return -1;
+}
+
+function findSum2(arr, value) {
+  const mapNum = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!mapNum[arr[i]]) {
+      mapNum.set(arr[i], i);
+    }
+
+    const found = value - arr[i];
+
+    if (mapNum.get(found)) {
+      return [arr[i], found];
+    }
+  }
+
+  return -1;
+}
+
+const arr = [1, 21, 3, 14, 5, 60, 7, 6];
+const value = 81;
+
+// console.log(findSum1(arr, value));
+console.log(findSum2(arr, value));
