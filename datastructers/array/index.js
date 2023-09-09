b9de0917550bad1ecc07c7635bb8d5d65bd296aa
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-09 23:29:34 +0700

    reverse string

--- datastructers/array/index.js
+++ datastructers/array/index.js
@@ -1,9 +1,21 @@
-const array = [1, 3, 5, 'seven'];
-console.log(array);
-// array.push(9);
-// array.pop();
-// array.shift();
-// array.unshift(0);
-// delete array[3];
-array.reverse();
-console.log(array);
+function reverseStr(str) {
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'that is not good';
+  }
+
+  let arr = str.split('');
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr.join('');
+}
+
+let str = 'Hello my name is Nhat';
+
+console.log(reverseStr(str));
