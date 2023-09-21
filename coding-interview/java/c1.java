710e9cb50b4be91b34cd3045c5a8bad71ad3ab9f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-21 22:37:59 +0700

    find all pairs java

--- coding-interview/java/c1.java
+++ coding-interview/java/c1.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * c1
  */
 public class c1 {
 
+    // c1
     public static int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -22,6 +27,7 @@ public class c1 {
         return null;
     }
 
+    // c2
     public static int findDuplicate(int[] numbers, int n) {
         BitSet bitSet = new BitSet(n);
 
@@ -36,11 +42,32 @@ public class c1 {
         return -1;
     }
 
+    // c3
+    public static List<List<Integer>> findAllPairs(int[] numbers, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int found = sum - numbers[i];
+
+            if (set.contains(found)) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(found);
+                pair.add(numbers[i]);
+                result.add(pair);
+            } else {
+                set.add(numbers[i]);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] numbers = new int[] { 2, 7, 2, 5, 7, 15 };
+        int[] numbers = new int[] { 2, 5, 7, 15, 13, 8 };
 
         // System.out.println("Test 1 : " + Arrays.toString(twoSum(numbers, 22)));
-        System.out.println("Test 2 : " + findDuplicate(numbers, 32000));
-
+        // System.out.println("Test 2 : " + findDuplicate(numbers, 32000));
+        System.out.println("Test 3 : " + findAllPairs(numbers, 20));
     }
 }
--- coding-interview/js/c1.js
+++ coding-interview/js/c1.js
@@ -1,6 +1,5 @@
 function twoSum(array, target) {
   let map = new Map();
-
   for (let i = 0; i < array.length; i++) {
     const found = target - array[i];
     if (map.has(found)) {
@@ -10,7 +9,39 @@ function twoSum(array, target) {
     }
   }
 
-  return -1;
+  return null;
 }
 
-console.log(twoSum([2, 7, 11, 15], 22));
+function findDuplicate(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (newArray[element]) {
+      return element;
+    } else {
+      newArray[element] = true;
+    }
+  }
+
+  return null;
+}
+
+function findAllPairs(array, target) {
+  let result = [];
+  let set = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const element = target - array[i];
+    if (set.has(element)) {
+      result.push([element, array[i]]);
+    } else {
+      set.add(array[i]);
+    }
+  }
+
+  return result;
+}
+
+// console.log(twoSum([2, 7, 11, 15], 13));
+// console.log(findDuplicate([2, 7, 11, 2, 15, 7, 2]));
+console.log(findAllPairs([2, 7, 10, 4, 15, 7, 2], 14));
