c11013306e2159f13772f5efdb8646cd29373781
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-01 10:18:58 +0700

    findDuplicates js

--- coding-interview/js/c1.js
+++ coding-interview/js/c1.js
@@ -1,6 +1,5 @@
-function twoSum(array, target) {
+function twoSum(array, target, lengthOfArray = array.length) {
   const mapLooped = new Map();
-  const lengthOfArray = array.length;
 
   for (let i = 0; i < lengthOfArray; i++) {
     const foundIndex = target - array[i];
@@ -13,5 +12,19 @@ function twoSum(array, target) {
 
   return null;
 }
+// console.log(twoSum([2, 7, 11, 15], 9));
 
-console.log(twoSum([2, 7, 11, 15], 9));
+function findDuplicates(array) {
+  const arrayLooped = [];
+
+  for (const number of array) {
+    if (arrayLooped[number]) {
+      return number;
+    }
+    arrayLooped[number] = true;
+  }
+
+  return null;
+}
+
+console.log(findDuplicates([1, 2, 3, 4, 3, 4]));
