2378b3154b4c26f7ddedb7c7bf760ca30bf828f9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-03 00:23:39 +0700

    remove even

--- datastructers/array/c1.js
+++ datastructers/array/c1.js
@@ -1,26 +1,9 @@
 // Challenge 1: Remove Even Integers From an Array
-function removeEvenNum1(array) {
-  const oddNum = [];
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if (element % 2 !== 0) {
-      oddNum.push(element);
-    }
-  }
 
-  return oddNum;
+function removeEven(numbers) {
+  return numbers.filter((num) => num % 2 !== 0);
 }
 
-function removeEvenNum2(array) {
-  return array.filter((number) => number % 2 !== 0);
-}
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-function removeEvenNum3(array) {
-  return array.reduce((pre, cur) => (cur % 2 !== 0 ? [...pre, cur] : pre), []);
-}
-
-const array = [1, 2, 4, 5, 10, 6, 3]; // [1,5,3]
-
-console.log(removeEvenNum1(array));
-console.log(removeEvenNum2(array));
-console.log(removeEvenNum3(array));
+console.log(removeEven(numbers));
