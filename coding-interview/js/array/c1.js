89cffc1fe444a2299f2e139ac4c5ba1123df3729
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-11 14:41:21 +0700

    Best Time to Buy And Sell A Stock

--- coding-interview/js/array/c1.js
+++ coding-interview/js/array/c1.js
@@ -1,41 +1,25 @@
-// Best time to buy and sell a stock
-
-function maxProfit(arr) {
-  const length = arr.length;
+// Best Time to Buy And Sell A Stock
+function maxProfit(prices) {
+  const length = prices.length;
   let max = 0;
-  let end = arr[length - 1];
+  let i = 0,
+    j = 1;
 
-  for (let i = length - 2; i >= 0; i--) {
-    const price = end - arr[i];
-    if (price <= 0) {
-      end = arr[i];
+  while (j < length) {
+    const greZero = prices[j] - prices[i];
+    if (greZero >= 0) {
+      max = Math.max(greZero, max);
     } else {
-      max = Math.max(max, price);
+      i = j;
     }
+    j++;
   }
 
   return max;
 }
 
-function maxProfit2(arr) {
-  const length = arr.length;
-  let max = 0;
-  let first = arr[0];
+const prices1 = [7, 1, 5, 1, 15, 2, 17];
+const prices2 = [7, 6, 4, 3, 1];
 
-  for (let end = 1; end < length; end++) {
-    const price = arr[end] - first;
-    if (price <= 0) {
-      first = arr[end];
-    } else {
-      max = Math.max(max, price);
-    }
-  }
-
-  return max;
-}
-
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-console.log(maxProfit([7, 6, 4, 3, 1]));
-
-console.log(maxProfit2([7, 1, 5, 3, 6, 4]));
-console.log(maxProfit2([7, 6, 4, 3, 1]));
+console.log(maxProfit(prices1));
+console.log(maxProfit(prices2));
