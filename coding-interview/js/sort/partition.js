23d62583a959e71b6171903a5ef88a180d73be2f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-11 21:13:27 +0700

    two way partitioning

---
+++ coding-interview/js/sort/partition.js
@@ -0,0 +1,22 @@
+// two way partitioning
+function partition(arr) {
+  let pivot = arr[arr.length - 1];
+  let f = 0;
+  for (let c = 0; c < arr.length - 1; c++) {
+    if (arr[c] <= pivot) {
+      let temp = arr[f];
+      arr[f] = arr[c];
+      arr[c] = temp;
+      f++;
+    }
+  }
+  let temp = arr[f];
+  arr[f] = arr[arr.length - 1];
+  arr[arr.length - 1] = temp;
+
+  return arr;
+}
+
+const arr = [2, 5, 1, 6, 7, 8, 4];
+
+console.log(partition(arr));
