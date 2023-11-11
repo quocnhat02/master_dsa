69fe9fd6016a857695aedf6f1475c367238531d5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-11 22:24:23 +0700

    counting sort 1

--- coding-interview/js/sort/countingSort.js
+++ coding-interview/js/sort/countingSort.js
@@ -1,32 +1,24 @@
 // counting sort
 function countingSort(arr) {
-  let ma = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > ma) {
-      ma = arr[i];
-    }
-  }
+  const max = arr.reduce((res, cur) => Math.max(res, cur), 0);
 
-  let count = new Array(ma + 1).fill(0);
-  for (let i = 0; i < arr.length; i++) {
-    count[arr[i]]++;
+  const count = new Array(max + 1).fill(0);
+  for (const num of arr) {
+    count[num]++;
   }
   for (let i = 1; i < count.length; i++) {
     count[i] = count[i - 1] + count[i];
   }
-  let result = new Array(arr.length);
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let position = count[arr[i]];
-    result[position - 1] = arr[i];
-    count[arr[i]]--;
+  const result = new Array(arr.length);
+  for (const num of arr) {
+    const position = count[num] - 1;
+    result[position] = num;
+    count[num]--;
   }
-  for (let i = 0; i < result.length; i++) {
-    arr[i] = result[i];
-  }
+
+  return result;
 }
 
-const arr = [4, 2, 6, 8, 7, 5, 1, 3];
+const arr = [4, 2, 6, 4, 8, 7, 5, 7, 1, 3];
 
-countingSort(arr);
-
-console.log(arr);
+console.log(countingSort(arr));
