1e810b4cad484649b8a1b257d0ce89f2c07ffa14
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-12 23:59:49 +0700

    linklist Insertion at Tail

--- datastructers/linklist/index.js
+++ datastructers/linklist/index.js
@@ -8,6 +8,7 @@ class Node {
 class LinkList {
   constructor() {
     this.head = null;
+    this.tail = null;
   }
 
   isEmpty() {
@@ -29,18 +30,35 @@ class LinkList {
     const newNode = new Node(data);
     if (this.isEmpty()) {
       this.head = newNode;
+      this.tail = newNode;
     } else {
       const head = this.head;
       newNode.nextElement = head;
       this.head = newNode;
     }
   }
+
+  insertTail(data) {
+    const newNode = new Node(data);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextElement = newNode;
+      this.tail = newNode;
+    }
+  }
 }
 
 const link_list = new LinkList();
 
-link_list.insertHead(1);
-link_list.insertHead(2);
-link_list.insertHead(3);
+// link_list.insertHead(1);
+// link_list.insertHead(2);
+// link_list.insertHead(3);
+link_list.insertTail(1);
+link_list.insertTail(2);
+link_list.insertTail(3);
 
 console.log(link_list.display());
+console.log(link_list.head);
+console.log(link_list.tail);
