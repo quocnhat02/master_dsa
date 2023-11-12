4f2af40b6129562f92d2932bd2afe73ac559545a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-13 00:26:06 +0700

    linklist Insertion at Nth

--- datastructers/linklist/index.js
+++ datastructers/linklist/index.js
@@ -9,6 +9,7 @@ class LinkList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
   }
 
   isEmpty() {
@@ -36,6 +37,7 @@ class LinkList {
       newNode.nextElement = head;
       this.head = newNode;
     }
+    this.length++;
   }
 
   insertTail(data) {
@@ -46,6 +48,35 @@ class LinkList {
     } else {
       this.tail.nextElement = newNode;
       this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  findIndex(index) {
+    let element = this.head;
+    let count = 1;
+
+    while (count < index) {
+      element = element.nextElement;
+      count++;
+    }
+
+    return element;
+  }
+
+  insertIndex(index, data) {
+    const newNode = new Node(data);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (index <= 0) {
+      this.insertHead(data);
+    } else if (index > this.length) {
+      this.insertTail(data);
+    } else {
+      const findNode = this.findIndex(index);
+      newNode.nextElement = findNode.nextElement;
+      findNode.nextElement = newNode;
     }
   }
 }
@@ -58,7 +89,10 @@ const link_list = new LinkList();
 link_list.insertTail(1);
 link_list.insertTail(2);
 link_list.insertTail(3);
+link_list.insertTail(4);
+link_list.insertIndex(3, 5);
 
 console.log(link_list.display());
-console.log(link_list.head);
-console.log(link_list.tail);
+// console.log(link_list.head);
+// console.log(link_list.tail);
+// console.log(link_list.findIndex(3));
