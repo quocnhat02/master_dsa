039989609336492a4e0de01a56caaf0b1d2d8c44
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-12 19:37:53 +0700

    Challenge 1: Remove Even Integers From an Array

--- datastructers/array/c1.js
+++ datastructers/array/c1.js
@@ -1,9 +1,9 @@
 // Challenge 1: Remove Even Integers From an Array
 
 function removeEven(numbers) {
-  return numbers.filter((num) => num % 2 !== 0);
+  return numbers.filter((number) => number % 2 !== 0);
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 console.log(removeEven(numbers));
