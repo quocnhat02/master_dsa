1e5e17d2fb04ecac014646458b6cd03791a75552
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-18 15:31:54 +0700

    Generate Binary Numbers From 1 to n Using Queue

---
+++ datastructers/queue/c1.js
@@ -0,0 +1,27 @@
+// Challenge 1:​ Generate Binary Numbers From 1 to n Using Queue
+
+function findBin(n) {
+  const result = [];
+  if (n < 1) {
+    return result;
+  }
+
+  let bin = 1;
+  result.push(bin.toString());
+
+  for (let i = 1; i < n; i++) {
+    if (bin % 10 === 0 && bin > 1) {
+      bin += 1;
+    } else {
+      bin *= 10;
+    }
+
+    result.push(bin.toString());
+  }
+
+  return result;
+}
+
+const n = 7;
+
+console.log(findBin(n));
