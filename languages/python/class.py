707c9817dd29a3043b4b0459d8164f997a43f71e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-19 14:55:37 +0700

    magic method

--- languages/python/class.py
+++ languages/python/class.py
@@ -3,11 +3,18 @@ class Robot:
         self.x = x
         self.y = y
 
+    # @classmethod
+    # def specific(cls):
+    #     return cls(1.1,4.6)
+
+    def __str__(self):
+        return f"I am a magic method ({self.x}:{self.y})"
+
     def walk(self):
         print(f"The robot is walking on {self.x}:{self.y}")
 
-robot = Robot(2.5,6.5)
+robot = Robot(1,2)
 
-robot.walk()
+print(str(robot))
 
 # print(isinstance(robot_obj,Robot))
