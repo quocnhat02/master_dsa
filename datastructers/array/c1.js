121dffa99c825454ae8a0384c21b42f1b17c9864
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-12-13 13:36:46 +0700

    Challenge 1: Remove Even Integers From an Array

--- datastructers/array/c1.js
+++ datastructers/array/c1.js
@@ -1,9 +1,7 @@
 // Challenge 1: Remove Even Integers From an Array
 
-function removeEven(numbers) {
-  return numbers.filter((number) => number % 2 !== 0);
+function removeEven(arr) {
+  return arr.filter((num) => num % 2 !== 0);
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-
-console.log(removeEven(numbers));
+console.log(removeEven([1, 2, 4, 5, 10, 6, 3]));
--- datastructers/array/c5.js
+++
@@ -1,9 +0,0 @@
-// Challenge 5: Find Minimum Value in Array
-
-function findMin(numbers) {
-  return numbers.reduce((res, cur) => Math.min(res, cur));
-}
-
-const arr = [9, 2, 3, 6];
-
-console.log(findMin(arr));
