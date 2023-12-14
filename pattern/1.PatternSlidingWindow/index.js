fda9643ea886e369c87eecb3b13f9bc0a09d9880
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-12-14 20:56:58 +0700

    Sliding Window

--- pattern/1.PatternSlidingWindow/index.js
+++ pattern/1.PatternSlidingWindow/index.js
@@ -1,31 +1,21 @@
-function findAverageOfSizeK(arr, k) {
-  if (!arr || k <= 0 || arr.length < k || !Array.isArray(arr)) {
-    return null;
-  }
+// Sliding Window
 
-  const length = arr.length;
-  const averages = [];
-  let windowStart = 0;
-  let windowSum = 0;
+function findAverageSubArr(array, k) {
+  const res = [];
+  let start = 0;
+  let sum = 0;
 
-  for (let windowEnd = 0; windowEnd < length; windowEnd++) {
-    windowSum += arr[windowEnd];
-
-    if (windowEnd >= k - 1) {
-      averages.push(windowSum / k);
-      windowSum -= arr[windowStart];
-      windowStart++;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    if (i >= k - 1) {
+      res.push(sum / k);
+      sum -= array[start++];
     }
   }
 
-  return averages;
+  return res;
 }
 
-/**
- * k = 5
- * averages = [11/5,]
- * i = 1, j = 6
- * sum = 14
- */
+console.log(findAverageSubArr([1, 3, 2, 6, -1, 4, 1, 8, 2], 5));
 
-console.log(findAverageOfSizeK([1, 3, 2, 6, -1, 4, 1, 8, 2], 5));
+// [2.2, 2.8, 2.4, 3.6, 2.8]
