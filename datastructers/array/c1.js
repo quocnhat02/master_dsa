7f0ff86cdea40eaf6240ca1f9585061109f1b961
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2024-01-22 09:40:14 +0700

    c1 rm even numbers

--- datastructers/array/c1.js
+++ datastructers/array/c1.js
@@ -1,7 +1,6 @@
 // Challenge 1: Remove Even Integers From an Array
-
-function removeEven(arr) {
-  return arr.filter((num) => num % 2 !== 0);
+function rmEvenNum(numbers) {
+  return numbers.filter((num) => num % 2 !== 0);
 }
 
-console.log(removeEven([1, 2, 4, 5, 10, 6, 3]));
+console.log(rmEvenNum([1, 2, 3, 4, 5, 6]));
--- datastructers/array/c5.js
+++
@@ -1,7 +0,0 @@
-// Challenge 5: Find Minimum Value in Array
-
-function findMin(arr) {
-  return arr.reduce((prev, cur) => Math.min(prev, cur));
-}
-
-console.log(findMin([1, 2, 3, 5]));
