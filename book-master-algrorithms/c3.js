84052cb1482c5a03fe3baffa321de27eb07cfd55
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-24 19:50:18 +0700

    reverse array

---
+++ book-master-algrorithms/c3.js
@@ -0,0 +1,36 @@
+/**
+ * Given an array of elements, reverse its elements
+ * so that the first element becomes the last,
+ * the second element becomes the second-to-last, and so on.
+ */
+
+function reverseArray(array) {
+  let left = 0,
+    right = array.length - 1;
+
+  while (left < right) {
+    let temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+
+    left++;
+    right--;
+  }
+
+  return array;
+}
+
+function reverseArrayWithExtra(array) {
+  const length = array.length;
+  const reversed = [];
+
+  for (let idx = length - 1; idx >= 0; idx--) {
+    const element = array[idx];
+    reversed.push(element);
+  }
+
+  return reversed;
+}
+
+console.log(reverseArray([1, 4, 2, 7, 5]));
+console.log(reverseArrayWithExtra([1, 4, 2, 7, 5]));
