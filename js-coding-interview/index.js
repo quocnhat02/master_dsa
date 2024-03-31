58db66d960381164faf290fd7244ce35fa6091bb
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-31 22:36:07 +0700

    binary to decimal

--- js-coding-interview/index.js
+++ js-coding-interview/index.js
@@ -1,9 +1,21 @@
 function decimalToBinary(val) {
   if (val <= 1) {
     return String(val);
-  } else {
-    return decimalToBinary(Math.floor(val / 2)) + decimalToBinary(val % 2);
   }
+  return decimalToBinary(Math.floor(val / 2)) + decimalToBinary(val % 2);
 }
 
-console.log(decimalToBinary(11));
+function binaryToDecimal(val) {
+  let decimal = 0,
+    idx = 0;
+  while (val !== 0) {
+    decimal += (val % 10) * Math.pow(2, idx);
+    val = Math.floor(val / 10);
+    idx++;
+  }
+
+  return decimal;
+}
+
+console.log(decimalToBinary(10));
+console.log(binaryToDecimal(1010));
