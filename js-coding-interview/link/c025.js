834c26cab2d39017374fc5f540061dbb67719be3
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-31 09:34:53 +0700

    link list isEmpty insert 3

--- js-coding-interview/link/c025.js
+++ js-coding-interview/link/c025.js
@@ -14,23 +14,37 @@ class LinkedList {
     return !this.head;
   }
 
+  calcLength() {
+    let currentNode = this.head;
+    let count = 0;
+    while (currentNode) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return count;
+  }
+
   insertAtTail(data) {
     const newNode = new Node(data);
-    if (this.isEmpty) {
+    const checkIsEmpty = this.isEmpty();
+    if (checkIsEmpty) {
       this.head = newNode;
     } else {
       let currentNode = this.head;
       while (currentNode.next) {
         currentNode = currentNode.next;
       }
-      currentNode = newNode;
+      currentNode.next = newNode;
     }
   }
+
+  insertAtHead(data) {}
 }
 
 const linked_list = new LinkedList();
 console.log(linked_list.isEmpty());
 
 linked_list.insertAtTail(1);
-console.log(linked_list.isEmpty());
+linked_list.insertAtTail(2);
+linked_list.insertAtTail(3);
 console.log(linked_list);
