5525838c176df080edbca04a68b02536fe48149a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-31 12:21:34 +0700

    link list implete stack 2

--- js-coding-interview/stack/index.js
+++ js-coding-interview/stack/index.js
@@ -3,6 +3,18 @@ class Stack {
     this.items = [];
     this.top = null;
   }
+
+  isEmpty() {
+    return !this.items.length;
+  }
+
+  getTop() {
+    return this.top;
+  }
+
+  size() {
+    return this.items.length;
+  }
 }
 
 const myStack = new Stack();
