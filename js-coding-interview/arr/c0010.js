2cf35fbeffcfcda3dcf1e92778cbed4b856f80e9
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-31 07:52:55 +0700

    rearrange

--- js-coding-interview/arr/c0010.js
+++ js-coding-interview/arr/c0010.js
@@ -3,5 +3,8 @@
 function findMin(arr) {
   return arr.reduce((acc, cur) => Math.min(acc, cur), arr[0]);
 }
-
+function rightRotate(arr, n) {
+  return arr.splice(arr.length - n).concat(arr.splice(0, arr.length));
+}
 console.log(findMin([9, 2, 3, 6]));
+console.log(rightRotate([1, 2, 3, 4, 5], 3));
---
+++ js-coding-interview/arr/c018.js
@@ -0,0 +1,23 @@
+function reArrange(arr) {
+  let idxLeftPos = 0,
+    tmp;
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    if (arr[idx] < 0) {
+      if (idx !== idxLeftPos) {
+        tmp = arr[idx];
+        arr[idx] = arr[idxLeftPos];
+        arr[idxLeftPos] = tmp;
+      }
+      idxLeftPos++;
+    }
+  }
+
+  return arr;
+}
+
+// [-1,-9,-6,4,5,10,20]
+//           ^
+//                   ^
+
+console.log(reArrange([10, -1, 20, 4, 5, -9, -6]));
