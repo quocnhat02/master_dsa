979782a6365da1dbf67753ed50795737b217fe7e
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-31 08:20:45 +0700

    sort max min 2

--- js-coding-interview/arr/c020.js
+++ js-coding-interview/arr/c020.js
@@ -10,7 +10,30 @@ function sortMaxMin(arr) {
   return maxMin;
 }
 
+function arrangeMaxMin(arr) {
+  let idxMax = arr.length - 1;
+  let idxMin = 0;
+  let maxElem = arr[idxMax] + 1;
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    if (idx % 2 === 0) {
+      arr[idx] += Math.floor((arr[idxMax] % maxElem) * maxElem);
+      idxMax--;
+    } else {
+      arr[idx] += Math.floor((arr[idxMin] % maxElem) * maxElem);
+      idxMin++;
+    }
+  }
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    arr[idx] = Math.floor(arr[idx] / maxElem);
+  }
+
+  return arr;
+}
+
 console.log(sortMaxMin([1, 2, 3, 4, 5]));
+console.log(arrangeMaxMin([1, 2, 3, 4, 5]));
 
 // [1,2,3,4,5]
 //      ^
