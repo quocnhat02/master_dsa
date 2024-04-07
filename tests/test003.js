a2b4105645a91f5eec85435a989f9551754ea721
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-07 18:08:51 +0700

    rotate

--- tests/test003.js
+++ tests/test003.js
@@ -9,4 +9,22 @@ function mergeArray(arr1, arr2) {
   //   return [...arr1, ...arr2];
 }
 
-console.log(mergeArray([1, 2, 3], [4, 5, 6]));
+// console.log(mergeArray([1, 2, 3], [4, 5, 6]));
+
+/*
+Xoay mảng:
+    Xoay mảng sang trái/phải một số lượng lần nhất định.
+    Đảo ngược mảng.
+*/
+
+function rotateLeft(arr, n) {
+  return arr.slice(-n).concat(arr.slice(0, arr.length - n));
+}
+
+console.log(rotateLeft([1, 2, 3, 4, 5], 3));
+
+function rotateRight(arr, n) {
+  return arr.slice(n).concat(arr.slice(0, n));
+}
+
+console.log(rotateRight([1, 2, 3, 4, 5], 3));
