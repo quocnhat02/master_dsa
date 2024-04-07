1ad78776f8bbc3acca9633b4173c4c79431c9faf
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-07 16:52:17 +0700

    find target

--- tests/test001.js
+++ tests/test001.js
@@ -9,3 +9,12 @@ const findMin = (arr) =>
   arr.reduce((acc, cur) => (acc < cur ? acc : cur), arr[0]);
 
 console.log(findMin([7, 4, 7, 9, 6, 8, 4, 2]));
+
+const findValue = (arr, target) => {
+  for (const number of arr) {
+    if (number === target) return true;
+  }
+  return false;
+};
+
+console.log(findValue([6, 4, 8, 9, 0, 3, 2], 3));
