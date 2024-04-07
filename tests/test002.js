8229c2e06edf688adca6be6e46cd5c5eaff26fce
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-07 16:59:57 +0700

    sort

--- tests/test002.js
+++ tests/test002.js
@@ -4,3 +4,15 @@ Sắp xếp:
     Sắp xếp mảng sử dụng các thuật toán Bubble Sort, Selection Sort, Insertion Sort, Merge Sort, Quick Sort, Radix Sort.
     Sắp xếp mảng sao cho tất cả các số chẵn nằm trước các số lẻ.
 */
+
+const sortAsc = (arr) => {
+  return arr.sort((a, b) => a - b);
+};
+
+console.log(sortAsc([5, 3, 8, 9, 0, 2, 4]));
+
+const sortDes = (arr) => {
+  return arr.sort((a, b) => b - a);
+};
+
+console.log(sortDes([5, 3, 8, 9, 0, 2, 4]));
