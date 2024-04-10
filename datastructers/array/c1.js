2b4d8bf5cfe87542932c50e48590e808cae7cb29
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-10 08:12:52 +0700

    setup

--- datastructers/array/c1.js
+++
@@ -1,6 +0,0 @@
-// Challenge 1: Remove Even Integers From an Array
-function rmEvenNum(numbers) {
-  return numbers.filter((num) => num % 2 !== 0);
-}
-
-console.log(rmEvenNum([1, 2, 3, 4, 5, 6]));
--- lang/python/index.py
+++
@@ -1,7 +0,0 @@
-# a, b, c = map(int, input('Nhap vao 3 so: ').split())
-
-# print(a, b, c)
-
-from math import *
-
-print(sqrt(4))
