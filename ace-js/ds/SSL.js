c8c000767307a5307d61648c84db97c167dc18cc
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-24 17:46:29 +0700

    ssl insert tail

--- ace-js/ds/SSL.js
+++ ace-js/ds/SSL.js
@@ -9,4 +9,57 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  isEmpty() {
+    return this.head === null ? true : false;
+  }
+
+  display() {
+    let result = '';
+    let currentNode = this.head;
+
+    while (currentNode.next !== null) {
+      result += currentNode.data + ' -> ';
+      currentNode = currentNode.next;
+    }
+
+    result += currentNode.data;
+
+    return result;
+  }
+
+  insertAtTail(data) {
+    const newNode = new Node(data);
+    const checkEmpty = this.isEmpty();
+
+    if (checkEmpty) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = newNode;
+    }
+
+    return newNode;
+  }
+
+  insertAtHead(data) {}
+
+  delete(data) {}
+
+  deleteAtHead() {}
+
+  search(data) {}
 }
+
+const linkedList = new LinkedList();
+
+linkedList.insertAtTail(1);
+linkedList.insertAtTail(2);
+linkedList.insertAtTail(3);
+
+console.log(linkedList.display());
