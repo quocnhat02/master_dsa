d7f0fd62ae0aa5ccd36a758cd3eb8133ff36c910
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-14 21:39:36 +0700

    find products

--- ace-js/ds/array.js
+++ ace-js/ds/array.js
@@ -1,16 +1,24 @@
-function findTwoSum(arr, value) {
-  const map = new Map();
+function findProducts(arr) {
+  const products = new Array(arr.length).fill(1);
+  let prev = 1 * arr[0];
+  let tmp = 1;
 
-  for (let i = 0; i < arr.length; i++) {
-    const elementSub = value - arr[i];
-    if (map.has(elementSub)) {
-      return [elementSub, arr[i]];
-    } else {
-      map.set(arr[i], i);
-    }
+  for (let i = 1; i < arr.length; i++) {
+    tmp *= prev;
+    products[i] = tmp;
+    prev = arr[i];
   }
 
-  return null;
+  prev = 1 * arr[arr.length - 1];
+  tmp = 1;
+
+  for (let i = arr.length - 2; i >= 0; i--) {
+    tmp *= prev;
+    products[i] *= tmp;
+    prev = arr[i];
+  }
+
+  return products;
 }
 
-console.log(findTwoSum([1, 21, 3, 3, 14, 5, 60, 7, 6], 6));
+console.log(findProducts([1, 2, 3, 4]));
