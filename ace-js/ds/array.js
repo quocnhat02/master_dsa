2c54268d9edd83b3eb44901ca928a2463e0657ad
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-14 21:46:51 +0700

    find products optimize

--- ace-js/ds/array.js
+++ ace-js/ds/array.js
@@ -1,21 +1,18 @@
 function findProducts(arr) {
   const products = new Array(arr.length).fill(1);
-  let prev = 1 * arr[0];
-  let tmp = 1;
+  let prev = 1 * arr[0],
+    tmp1 = 1,
+    after = 1 * arr[arr.length - 1],
+    tmp2 = 1;
 
   for (let i = 1; i < arr.length; i++) {
-    tmp *= prev;
-    products[i] = tmp;
+    tmp1 *= prev;
+    products[i] *= tmp1;
     prev = arr[i];
-  }
 
-  prev = 1 * arr[arr.length - 1];
-  tmp = 1;
-
-  for (let i = arr.length - 2; i >= 0; i--) {
-    tmp *= prev;
-    products[i] *= tmp;
-    prev = arr[i];
+    tmp2 *= after;
+    products[arr.length - 1 - i] *= tmp2;
+    after = arr[arr.length - 1 - i];
   }
 
   return products;
