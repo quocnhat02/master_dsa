b62fc8060e70dad39fc0df8eb1d4cbf37e9267f0
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-18 21:14:07 +0700

    SLL inserttail

--- ace-js/ds/SSL.js
+++ ace-js/ds/SSL.js
@@ -8,5 +8,40 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
   }
+
+  display() {
+    let head = this.head;
+    let result = '';
+
+    while (head.next !== null) {
+      result += head.data + '->';
+      head = head.next;
+    }
+
+    result += head.data;
+
+    return result;
+  }
+
+  insertAtTail(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insertAtHead(data) {}
 }
+
+const newLinkedList = new LinkedList();
+newLinkedList.insertAtTail(1);
+newLinkedList.insertAtTail(2);
+newLinkedList.insertAtTail(3);
+
+console.log(newLinkedList.display());
