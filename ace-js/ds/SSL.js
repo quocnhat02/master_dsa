b14339a50ba266cbb060dda91985059e3a31d4c5
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-18 22:12:42 +0700

    SLL op

--- ace-js/ds/SSL.js
+++ ace-js/ds/SSL.js
@@ -15,14 +15,37 @@ class LinkedList {
     let head = this.head;
     let result = '';
 
-    while (head.next !== null) {
-      result += head.data + '->';
-      head = head.next;
+    if (head == this.tail) {
+      result = head ? head.data : null;
+    } else {
+      while (head.next !== null) {
+        result += head.data + '->';
+        head = head.next;
+      }
+      result += head.data;
     }
 
-    result += head.data;
-
     return result;
+  }
+
+  isEmpty() {
+    return !this.head ? true : false;
+  }
+
+  search(data) {
+    let currentNode = this.head;
+    if (!currentNode) {
+      return null;
+    } else {
+      while (currentNode !== null) {
+        if (currentNode.data === data) {
+          return currentNode;
+        }
+        currentNode = currentNode.next;
+      }
+
+      return null;
+    }
   }
 
   insertAtTail(data) {
@@ -46,11 +69,51 @@ class LinkedList {
       this.head = newNode;
     }
   }
+
+  deleteAtHead() {
+    let flag = true;
+    if (this.head == this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      flag = true;
+      this.head = this.head.next;
+    }
+    return flag;
+  }
+
+  delete(data) {
+    let flag = false;
+    if (!this.head) {
+      flag = false;
+    } else if (this.head.data == data) {
+      return this.deleteAtHead();
+    } else {
+      let currentNode = this.head;
+      let prevNode = this.head;
+      while (currentNode !== null) {
+        if (currentNode.data === data) {
+          prevNode.next = currentNode.next;
+          if (currentNode.next === null) {
+            this.tail = prevNode;
+          }
+          flag = true;
+        }
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      return flag;
+    }
+  }
 }
 
 const newLinkedList = new LinkedList();
-newLinkedList.insertAtHead(1);
-newLinkedList.insertAtHead(2);
-newLinkedList.insertAtHead(3);
+// newLinkedList.insertAtTail(1);
+// newLinkedList.insertAtTail(2);
+// newLinkedList.insertAtTail(3);
+// newLinkedList.insertAtTail(4);
+// newLinkedList.insertAtTail(5);
 
 console.log(newLinkedList.display());
+console.log(newLinkedList.isEmpty());
