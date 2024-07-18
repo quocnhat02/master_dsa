964379cc7968393878a343bef3cacb1975f7e1dd
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-18 17:33:20 +0700

    rearrange pos and neg

--- ace-js/ds/array.js
+++ ace-js/ds/array.js
@@ -1,5 +1,14 @@
-function rotateRight(array, n) {
-  return array.splice(-n).concat(array);
+function rearrange(arr) {
+  let idx_swap = 0;
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    if (arr[idx] < 0) {
+      [arr[idx], arr[idx_swap]] = [arr[idx_swap], arr[idx]];
+      idx_swap++;
+    }
+  }
+
+  return arr;
 }
 
-console.log(rotateRight([1, 2, 3, 4, 5], 3));
+console.log(rearrange([10, -1, 20, 0, 4, 5, -9, -6]));
