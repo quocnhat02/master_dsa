3750bb4b27deb3e7074e124f7ee8379829fd0f92
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-10 12:28:58 +0700

    find sub sum sequence with sliding window

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function findSumSubSequence(arr, k) {
+  const length = arr.length;
+  let currentSum = 0;
+  let startWindow = 0;
+  let maxSum = arr[0];
 
+  for (let endWindow = 0; endWindow < length; endWindow++) {
+    currentSum += arr[endWindow];
+
+    if (endWindow - startWindow >= k - 1) {
+      maxSum = Math.max(maxSum, currentSum);
+      currentSum -= arr[startWindow];
+      startWindow++;
+    }
+  }
+
+  return maxSum;
+}
+
+console.log(findSumSubSequence([1, 2, 3, 4, 5], 3));
