4b7f281fe7ba982f16f5eef818ec0a7a3503932a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-10 12:44:50 +0700

    find longest sub string with sliding window

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function findFirstCharWithMoreOnce(str) {
-  let charSet = new Set();
+function findLongestSubStringSequence(str) {
+  const charCount = new Map();
 
-  for (let idx = 0; idx < str.length; idx++) {
-    if (charSet.has(str[idx])) {
-      return str[idx];
+  let startWindow = 0;
+  let longest = 0;
+
+  for (let endWindow = 0; endWindow < str.length; endWindow++) {
+    charCount.set(str[endWindow], (charCount.get(str[endWindow]) | 0) + 1);
+
+    while (charCount.get(str[endWindow]) > 1 && endWindow > startWindow) {
+      let element = charCount.get(str[endWindow]);
+      charCount.set(str[endWindow], element - 1);
+
+      if (element === 0) {
+        charCount.delete(str[endWindow]);
+      }
+
+      startWindow++;
     }
-    charSet.add(str[idx]);
+
+    longest = Math.max(longest, endWindow - startWindow + 1);
   }
 
-  return '';
+  return longest;
 }
 
-function findFirstCharWithMoreOnce2(str) {
-  for (let idx = 0; idx < str.length - 1; idx++) {
-    if (str[idx].indexOf(str.substring(idx + 1))) {
-      return str[idx];
-    }
-  }
-
-  return '';
-}
-
-console.log(findFirstCharWithMoreOnce('abcab'));
-console.log(findFirstCharWithMoreOnce2('abcab'));
+console.log(findLongestSubStringSequence('abcabcbb'));
