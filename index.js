4df8d3990bf3f5a11af3651ba85cfab8978ad84a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-11 18:07:52 +0700

    longest substring with k distinct

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-function smallest_sub_array_of_size_k(arr, s) {
-  let smallest = arr.length,
-    startWindow = 0,
-    currentSum = 0;
+function find_longest_sub_string_with_no_more_k_chars(str, k) {
+  const countChar = new Map();
+  let start = 0,
+    longest = 0;
 
-  for (let endWindow = 0; endWindow < arr.length; endWindow++) {
-    currentSum += arr[endWindow];
+  for (let end = 0; end < str.length; end++) {
+    const elmEnd = str.charAt(end);
+    countChar.set(elmEnd, (countChar.get(elmEnd) | 0) + 1);
 
-    while (currentSum >= s && endWindow >= startWindow) {
-      smallest = Math.min(smallest, endWindow - startWindow + 1);
-      currentSum -= arr[startWindow];
-      startWindow++;
+    while (countChar.size > k && end - start) {
+      const elmStart = str.charAt(start);
+      countChar.set(elmStart, countChar.get(elmStart) - 1);
+
+      if (!countChar.get(elmStart)) {
+        countChar.delete(elmStart);
+      }
+
+      start++;
     }
+
+    longest = Math.max(longest, end - start + 1);
   }
 
-  return smallest;
+  return longest;
 }
 
-console.log(smallest_sub_array_of_size_k([2, 1, 5, 2, 3, 2], 7));
-console.log(smallest_sub_array_of_size_k([2, 1, 5, 2, 8], 8));
-console.log(smallest_sub_array_of_size_k([3, 4, 1, 1, 6], 8));
+console.log(find_longest_sub_string_with_no_more_k_chars('araaci', 2));
+console.log(find_longest_sub_string_with_no_more_k_chars('araaci', 1));
+console.log(find_longest_sub_string_with_no_more_k_chars('cbbebi', 3));
