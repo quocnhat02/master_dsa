0da9e253d19bf5a69b26c276a9054cd3464172d7
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-11 18:21:57 +0700

    find no repeat substring

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-function find_max_fruits_into_basket(arr) {
-  const countFruits = new Map();
+function find_no_repeat_substring(str) {
+  const countChar = new Map();
   let start = 0,
-    max = 0;
+    longest = 0;
 
-  for (let end = 0; end < arr.length; end++) {
-    const elmEnd = arr[end];
-    countFruits.set(elmEnd, (countFruits.get(elmEnd) | 0) + 1);
+  for (let end = 0; end < str.length; end++) {
+    const elmEnd = str.charAt(end);
+    countChar.set(elmEnd, (countChar.get(elmEnd) | 0) + 1);
 
-    while (countFruits.size > 2 && end > start) {
-      const elmStart = arr[start];
-      countFruits.set(elmStart, countFruits.get(elmStart) - 1);
+    while (countChar.get(elmEnd) > 1 && end > start) {
+      const elmStart = str.charAt(start);
+      countChar.set(elmStart, countChar.get(elmStart) - 1);
 
-      if (!countFruits.get(elmStart)) {
-        countFruits.delete(elmStart);
+      if (!countChar.get(elmStart)) {
+        countChar.delete(elmStart);
       }
 
       start++;
     }
 
-    max = Math.max(max, end - start + 1);
+    longest = Math.max(longest, end - start + 1);
   }
 
-  return max;
+  return longest;
 }
 
-console.log(find_max_fruits_into_basket(['A', 'B', 'C', 'A', 'C']));
-console.log(find_max_fruits_into_basket(['A', 'B', 'C', 'B', 'B', 'C']));
+console.log(find_no_repeat_substring('aabccbb'));
+console.log(find_no_repeat_substring('abbbb'));
+console.log(find_no_repeat_substring('abccde'));
