967aa8d28dbb7bb8da223e3bf7ac99ed7c84c8af
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-18 13:32:34 +0700

    find two sum

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-function find_no_repeat_substring(str) {
-  const countChar = new Map();
-  let start = 0,
-    longest = 0;
+function findTwoSum(arr, target) {
+  const length = arr.length;
+  if (length < 2) {
+    return 'No';
+  }
+  const numSet = new Set();
 
-  for (let end = 0; end < str.length; end++) {
-    const elmEnd = str.charAt(end);
-    countChar.set(elmEnd, (countChar.get(elmEnd) | 0) + 1);
+  numSet.add(arr[0]);
 
-    while (countChar.get(elmEnd) > 1 && end > start) {
-      const elmStart = str.charAt(start);
-      countChar.set(elmStart, countChar.get(elmStart) - 1);
-
-      if (!countChar.get(elmStart)) {
-        countChar.delete(elmStart);
-      }
-
-      start++;
+  for (let idx = 1; idx < length; idx++) {
+    if (numSet.has(target - arr[idx])) {
+      return 'Yes';
     }
-
-    longest = Math.max(longest, end - start + 1);
+    numSet.add(arr[idx]);
   }
 
-  return longest;
+  return 'No';
 }
 
-console.log(find_no_repeat_substring('aabccbb'));
-console.log(find_no_repeat_substring('abbbb'));
-console.log(find_no_repeat_substring('abccde'));
+console.log(findTwoSum([1, -2, 1, 0, 5], 0));
+console.log(findTwoSum([0, -1, 2, -3, 1], -2));
