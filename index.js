dfd3e7eff1e8f1cd2a96bba8b4a42ff3a19b77d9
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-27 15:21:36 +0700

    Kiểm tra chuỗi đối xứng (Palindrome)

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-function findSubarrayWithKDistance(arr, k) {
-  return atMostK(arr, k) - atMostK(arr, k - 1);
-}
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-function atMostK(arr, k) {
-  const len = arr.length;
-  if (!len) {
-    return 0;
+  let left = 0,
+    right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
   }
 
-  let start = 0,
-    res = 0;
-  const numMap = new Map();
-
-  for (let end = 0; end < len; end++) {
-    numMap.set(arr[end], (numMap.get(arr[end]) || 0) + 1);
-
-    while (numMap.size > k && start < end) {
-      numMap.set(arr[start], numMap.get(arr[start]) - 1);
-      if (numMap.get(arr[start]) === 0) numMap.delete(arr[start]);
-      start++;
-    }
-
-    res += end - start + 1;
-  }
-
-  return res;
+  return true;
 }
 
-// PT(X) = 1 + 2 + ... + (X - 1) + X
-// PT(X-1) = 1 + 2 + ... + (X - 1)
-
-// Ví dụ sử dụng:
-console.log(findSubarrayWithKDistance([1, 2, 1, 2, 3], 2)); // Đầu ra: 7
-console.log(findSubarrayWithKDistance([1, 2, 1, 3, 4], 3)); // Đầu ra: 3
-console.log(findSubarrayWithKDistance([1, 2, 3, 4], 3)); // Đầu ra: 2
+// Ví dụ sử dụng
+console.log(isPalindrome('A man, a plan, a canal: Panama')); // true
+console.log(isPalindrome('race a car')); // false
