a81f55dbba0b22f9f4b13713ce150fea41f71ba6
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-27 15:57:01 +0700

    merge sort

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-function removeElement(arr, k) {
-  let i = 0;
+function mergeSortedArrays(arr1, arr2) {
+  const len = arr1.length + arr2.length;
+  const merged = [];
 
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[j] !== k) {
-      arr[i] = arr[j];
-      i++;
+  let start1 = 0,
+    start2 = 0;
+
+  while (start1 + start2 < len) {
+    if (!arr2[start2] || arr1[start1] < arr2[start2]) {
+      merged.push(arr1[start1]);
+      start1++;
+    } else {
+      merged.push(arr2[start2]);
+      start2++;
     }
   }
 
-  return i;
+  return merged;
 }
 
 // Ví dụ sử dụng
-let nums = [3, 2, 2, 3];
-console.log(removeElement(nums, 3)); // Output: 2
-console.log(nums.slice(0, 2)); // Output: [2, 2]
+let nums2 = [1, 1, 3, 4, 7, 9];
+let nums1 = [2, 5, 6];
+console.log(mergeSortedArrays(nums1, nums2));
+// Output: [1, 2, 2, 3, 5, 6]
