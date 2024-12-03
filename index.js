166fff779e915951b75090ae8c0401f280f406e5
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-03 22:01:01 +0700

    two sum

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-function reverseArray(array) {
-  if (array.length <= 1) {
-    return array;
+function twoSum(array, target) {
+  if (array.length === 0 || target == undefined || target === null) {
+    return null;
+  }
+  const map = new Map();
+  for (let idx = 0; idx < array.length; idx++) {
+    const complement = target - array[idx];
+    if (map.has(complement)) return [array[map.get(complement)], array[idx]];
+    map.set(array[idx], idx);
   }
 
-  let reversed = [];
-  let left = 0,
-    right = array.length - 1;
-
-  while (left <= right) {
-    if (left == right) {
-      reversed[left] = array[left];
-    } else {
-      reversed[left] = array[right];
-      reversed[right] = array[left];
-    }
-    left++;
-    right--;
-  }
-
-  return reversed;
+  return null;
 }
 
-// function reverseArray(str) {
-//   return str.reverse();
-// }
-
-console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(twoSum([1, 2, 3, 4, 5], 8));
