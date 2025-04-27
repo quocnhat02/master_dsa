1903087c11eba8487d44bc927793df8e31da8365
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-04-27 14:57:00 +0700

    find max average subarray

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-function lengthOfLongestSubStrWithoutRepeat(s) {
-  if (s.length == 0) {
+// Given array of positive integer numbers and a positive number k,
+// find the maximum sum of any contiguous subaaray of size k
+
+// arr= [1, 2, 3, 4, 2, 5, 3], k = 2 -> 8
+// idx = 0, sum = 0, max = 0
+
+function findAverageSumSubarray(nums, k) {
+  if (nums.length < k) {
     return 0;
   }
-  const charMap = new Set();
-  let longest = 0;
-  let sub_idx = 0;
 
-  for (let idx = 0; idx < s.length; idx++) {
-    const element = s[idx];
-    while (charMap.has(element)) {
-      charMap.delete(s[sub_idx]);
-      sub_idx++;
-    }
-    charMap.add(element);
-    longest = Math.max(longest, idx - sub_idx + 1);
+  let pre_idx = 0, sum = 0, max = 0;
+
+  for (let idx = 0; idx < k; idx++) {
+    sum += nums[idx]
   }
 
-  return longest;
+  max = sum / k;
+
+  for (let idx = k; idx < nums.length; idx++) {
+    sum = sum - nums[pre_idx++] + nums[idx] 
+    max = Math.max(max, sum / k)
+  }
+
+  return max;
+
 }
 
-// 'abcbdaf' -> 'cbdaf' : 5
-console.log(lengthOfLongestSubStrWithoutRepeat('abcbdaf'));
-console.log(lengthOfLongestSubStrWithoutRepeat('bbbbb'));
+console.log(findAverageSumSubarray([1, 2, 3, 4, 2, 5, 3], 2));
