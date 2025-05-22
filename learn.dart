42c0fd2da9d557787d2ca074223da0ada5610f85
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-05-22 20:18:40 +0700

    flutter generics

--- learn.dart
+++ learn.dart
@@ -1,8 +1,13 @@
-void main() {
-  List<String> names = ["a", "b"];
-
-  for (int i = 0; i < names.length; i++) {
-    String elm = names[i];
+void printList<T>(List<T> arr) {
+  for (int i = 0; i < arr.length; i++) {
+    T elm = arr[i];
     print("$elm");
   }
+}
+
+void main() {
+  List<String> names = ["a", "b"];
+  List<int> numbers = [1, 2];
+
+  printList(numbers);
 }
