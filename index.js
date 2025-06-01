355b4a5cc13d541616233a9a6c8a32c81a2805aa
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 11:11:54 +0700

    removeNthNodeFromEnd

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-function threeSum(array) {
-  array = array.sort((a, b) => a - b);
-  const result = [];
-
-  for (let idx = 0; idx < array.length - 2; idx++) {
-    if (array[idx] === array[idx + 1]) {
-      continue;
-    }
-
-    let left = idx + 1,
-      right = array.length - 1;
-    while (left < right) {
-      const sum = array[idx] + array[left] + array[right];
-
-      if (sum === 0) {
-        result.push(array[idx], array[left], array[right]);
-        while (array[left] === array[left + 1]) left++;
-        while (array[right] === array[right - 1]) right--;
-        left++;
-        right--;
-      } else if (sum < 0) left++;
-      else right--;
-    }
+class ListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
   }
-
-  return result;
 }
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+function removeNthNodeFromEnd(head, n) {
+  let tempList = new ListNode(0);
+  tempList.next = head;
+
+  let slow = tempList;
+  let fast = tempList;
+
+  for (let idx = 0; idx < n; idx++) {
+    fast = fast.next;
+  }
+
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  slow.next = slow.next.next;
+
+  return tempList.next;
+}
+
+function printList(head) {
+  let res = [];
+  while (head) {
+    res.push(head.value);
+    head = head.next;
+  }
+  console.log(res.join(' -> '));
+}
+
+let head = new ListNode(
+  1,
+  new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+);
+printList(removeNthNodeFromEnd(head, 2));
