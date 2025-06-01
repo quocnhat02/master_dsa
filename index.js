fbd7c21ee9ea3a966bc216f5d05f8ebd3ba411fb
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 12:46:16 +0700

    validWordAbbreviation

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function reverseString(s) {
-  return s.trim().split(/\s+/).reverse().join(' ');
+function validWordAbbreviation(word, abbr) {
+  let i = 0,
+    j = 0;
+
+  while (i < word.length && j < abbr.length) {
+    if (isNaN(abbr[j])) {
+      if (word[i] !== abbr[j]) return false;
+      i++;
+      j++;
+    } else {
+      if (abbr[j] === '0') return false;
+
+      let num = 0;
+      while (j < abbr.length && !isNaN(abbr[j])) {
+        num = num * 10 + parseInt(abbr[j]);
+        j++;
+      }
+      i += num;
+    }
+  }
+
+  return i === word.length && j === abbr.length;
 }
 
-console.log(reverseString('  the sky  is  blue  '));
+console.log(validWordAbbreviation('internationalization', 'i12iz4n')); // true
+console.log(validWordAbbreviation('apple', 'a2e')); // false
