51f2c8a4f479d4304af2356d8fc21e6cd8ba0039
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 11:21:47 +0700

    sortColors

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-class ListNode {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
+function sortColors(array) {
+  let left = 0,
+    mid = 0,
+    right = array.length - 1;
+
+  while (mid <= right) {
+    if (array[mid] === 0) {
+      [array[left], array[mid]] = [array[mid], array[left]];
+      mid++;
+      left++;
+    } else if (array[mid] === 1) {
+      mid++;
+    } else if (array[mid] === 2) {
+      [array[right], array[mid]] = [array[mid], array[right]];
+      right--;
+    }
   }
+  return array;
 }
 
-function removeNthNodeFromEnd(head, n) {
-  let tempList = new ListNode(0);
-  tempList.next = head;
-
-  let slow = tempList;
-  let fast = tempList;
-
-  for (let idx = 0; idx < n; idx++) {
-    fast = fast.next;
-  }
-
-  while (fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next;
-  }
-
-  slow.next = slow.next.next;
-
-  return tempList.next;
-}
-
-function printList(head) {
-  let res = [];
-  while (head) {
-    res.push(head.value);
-    head = head.next;
-  }
-  console.log(res.join(' -> '));
-}
-
-let head = new ListNode(
-  1,
-  new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
-);
-printList(removeNthNodeFromEnd(head, 2));
+console.log(sortColors([2, 0, 2, 1, 1, 0]));
