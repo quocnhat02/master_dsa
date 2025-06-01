9d20024c81f38c546cc37cde418774d065472302
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 16:03:20 +0700

    middleNode

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function hasCycle(head) {
+function middleNode(head) {
   let slow = head;
   let fast = head;
 
   while (fast && fast.next) {
     slow = slow.next;
     fast = fast.next.next;
-    if (slow === fast) return true;
   }
-  return false;
+
+  return slow;
 }
