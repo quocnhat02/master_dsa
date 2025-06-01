ccb637621f6a70217a814be84a6d78674266f682
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 14:43:33 +0700

    hasCycle

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-function isStrobogrammatic(number) {
-  const map = {
-    0: '0',
-    1: '1',
-    6: '9',
-    8: '8',
-    9: '6',
-  };
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
 
-  let left = 0,
-    right = number.length - 1;
-  while (left < right) {
-    const l = number[left];
-    const r = number[right];
-    if (!(l in map) || map[l] !== r) {
-      return false;
-    }
-
-    left++;
-    right--;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-
-  return true;
+  return false;
 }
-
-console.log(isStrobogrammatic('69')); // true  → 69 → xoay thành 96 → đúng
-console.log(isStrobogrammatic('88')); // true  → xoay vẫn là 88
-console.log(isStrobogrammatic('962')); // false → 9→6, 6→9, nhưng 2 không hợp lệ
-console.log(isStrobogrammatic('1')); // true
-console.log(isStrobogrammatic('2')); // false
