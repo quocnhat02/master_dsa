cecb7397be413ab09519c93b3805416b3fa3da89
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 18:29:23 +0700

    sumSubarraySizeK

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
+function sumSubarraySizeK(arr, k) {
+  if (arr.length < k) {
+    return null;
+  }
+  let maxSum = 0,
+    currentSum = 0;
 
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-
-    if (slow === fast) {
-      slow = head;
-      while (slow !== fast) {
-        slow = slow.next;
-        fast = fast.next;
-      }
-
-      return slow;
-    }
+  for (let idx = 0; idx < k; idx++) {
+    currentSum += arr[idx];
   }
 
-  return null;
+  maxSum = currentSum;
+
+  for (let idx = k; idx < arr.length; idx++) {
+    currentSum = currentSum + arr[idx] - arr[idx - k];
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
 }
+
+console.log(sumSubarraySizeK([2, 1, 5, 1, 3, 2], 3));
