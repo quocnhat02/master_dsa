29394a6b8a243d90ec0664c1977f1b13a731b11e
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 10:09:28 +0700

    threeSum

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-function mostArea(height) {
-  let left = 0,
-    right = height.length - 1;
+function threeSum(array) {
+  array = array.sort((a, b) => a - b);
+  const result = [];
 
-  let maxArea = 0;
+  for (let idx = 0; idx < array.length - 2; idx++) {
+    if (array[idx] === array[idx + 1]) {
+      continue;
+    }
 
-  while (left < right) {
-    const area = Math.min(height[left], height[right]) * (right - left);
+    let left = idx + 1,
+      right = array.length - 1;
+    while (left < right) {
+      const sum = array[idx] + array[left] + array[right];
 
-    maxArea = Math.max(maxArea, area);
-
-    if (height[left] < height[right]) left++;
-    else right--;
+      if (sum === 0) {
+        result.push(array[idx], array[left], array[right]);
+        while (array[left] === array[left + 1]) left++;
+        while (array[right] === array[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) left++;
+      else right--;
+    }
   }
 
-  return maxArea;
+  return result;
 }
 
-console.log(mostArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
