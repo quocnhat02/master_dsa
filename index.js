ef03aad434372c2513e3d759e601e8c9d06f1649
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 09:06:08 +0700

    hasDuplicated

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-// You have give an array of characters where each character represents a fruit.
-// You have 2 baskets, find longest number of nums.
-// Each baskets can only hold a single type of fruit.
-
-//  numMap = {}, pre_idx = 0, longest = 0, k = 2
-// [01100010110] => 2
-
-function containPermutation(s1, s2) {
-  const length1 = s1.length;
-  const length2 = s2.length;
-
-  if (length1 > length2) {
-    return false;
-  }
-
-  let pre_idx = 0;
-
-  for (let idx = length2 - 1; idx >= 0; idx--) {
-    const rightElm = s2.charAt(idx);
-
-    if (rightElm == s1.charAt(pre_idx)) {
-      pre_idx++;
-    } else if (rightElm == s1.charAt(0)) {
-      pre_idx = 1;
-    } else {
-      pre_idx = 0;
-    }
-
-    if (pre_idx == length1) {
+function hasDuplicate(arr) {
+  let left = 0,
+    right = 1;
+  while (right < arr.length) {
+    if (arr[left] === arr[right]) {
       return true;
     }
+
+    left++;
+    right++;
   }
 
   return false;
 }
 
-console.log(containPermutation('ab', 'eidbbcaaoaoo'));
+console.log(hasDuplicate([1, 2, 2, 3, 4, 5]));
