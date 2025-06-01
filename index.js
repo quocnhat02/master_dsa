cbda8758cda0e7cf231d3e99576dfc3c3cdb940b
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-01 11:26:43 +0700

    reverseString

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function sortColors(array) {
-  let left = 0,
-    mid = 0,
-    right = array.length - 1;
-
-  while (mid <= right) {
-    if (array[mid] === 0) {
-      [array[left], array[mid]] = [array[mid], array[left]];
-      mid++;
-      left++;
-    } else if (array[mid] === 1) {
-      mid++;
-    } else if (array[mid] === 2) {
-      [array[right], array[mid]] = [array[mid], array[right]];
-      right--;
-    }
-  }
-  return array;
+function reverseString(s) {
+  return s.trim().split(/\s+/).reverse().join(' ');
 }
 
-console.log(sortColors([2, 0, 2, 1, 1, 0]));
+console.log(reverseString('  the sky  is  blue  '));
