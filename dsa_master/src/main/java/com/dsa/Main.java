8424ec1541cf7994a594597f04e85d4530424b97
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-14 10:53:31 +0700

    sort

---
+++ dsa_master/src/main/java/com/dsa/Main.java
@@ -0,0 +1,18 @@
+package com.dsa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] nums = {1,5,7,4,8};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
