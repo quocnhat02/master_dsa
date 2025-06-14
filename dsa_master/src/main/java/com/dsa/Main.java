18af40e87fcb8fe46ae662756b9b8385b261d44b
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-14 10:57:25 +0700

    sort 3

--- dsa_master/src/main/java/com/dsa/Main.java
+++ dsa_master/src/main/java/com/dsa/Main.java
@@ -1,18 +1,35 @@
 package com.dsa;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Comparator;
 
+class SinhVien {
+    String ten;
+    int diem;
+
+    public SinhVien(String ten, int diem) {
+        this.ten = ten;
+        this.diem = diem;
+    }
+
+    public String toString() {
+        return ten + ": " + diem;
+    }
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        String[] names = {"Nhat", "Light", "Anh", "Phuc"};
-        Arrays.sort(names);
-        System.out.println(Arrays.toString(names));
+        SinhVien[] ds = {
+                new SinhVien("An", 8),
+                new SinhVien("Bình", 9),
+                new SinhVien("Lan", 7)
+        };
+
+        Arrays.sort(ds, Comparator.comparingInt(sv -> sv.diem));
+        System.out.println(Arrays.toString(ds));
     }
 
 }
