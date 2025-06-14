848de7827b665d7eb693c3d206b9ba31e6e5934a
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-14 11:52:41 +0700

    two pointer 1

--- dsa_master/src/main/java/com/dsa/Main.java
+++ dsa_master/src/main/java/com/dsa/Main.java
@@ -4,32 +4,34 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
-class SinhVien {
-    String ten;
-    int diem;
 
-    public SinhVien(String ten, int diem) {
-        this.ten = ten;
-        this.diem = diem;
-    }
-
-    public String toString() {
-        return ten + ": " + diem;
-    }
-}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) {
-        SinhVien[] ds = {
-                new SinhVien("An", 8),
-                new SinhVien("Bình", 9),
-                new SinhVien("Lan", 7)
-        };
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
 
-        Arrays.sort(ds, Comparator.comparingInt(sv -> sv.diem));
-        System.out.println(Arrays.toString(ds));
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+
+        System.out.println(Arrays.toString((twoSum(nums, 5))));
     }
 
 }
